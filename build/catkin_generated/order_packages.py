5619bca80667281418bf3dcf490622f73f1cab01
vuweed <halucminhvu2000@gmail.com>
2023-05-06 16:30:29 +0700

    first commit

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/vuweed/Documents/ROS/demo/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/vuweed/interbotix_ws/devel;/opt/ros/noetic'.split(';') if '/home/vuweed/interbotix_ws/devel;/opt/ros/noetic' != '' else []
